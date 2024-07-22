bb897508d8a31fdd9951307d151f4ff4a7c4653d
niyatimadaan <madaanniyati@gmail.com>
2024-07-22 23:17:31 +0530

    port on render

--- ai_agent/chatbot2.py
+++ ai_agent/chatbot2.py
@@ -131,4 +131,5 @@ def ask_question():
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    # app.run(port=5000) # For running on localhost
+    app.run(host="0.0.0.0", port=5000) # For running on Docker container
