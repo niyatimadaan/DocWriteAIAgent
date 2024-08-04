8b7c67aee844d0b5fa0981c82088aa300d180574
niyatimadaan <madaanniyati@gmail.com>
2024-08-04 20:06:40 +0530

    button message change

--- next-tinymce/src/app/newDoc/document.tsx
+++ next-tinymce/src/app/newDoc/document.tsx
@@ -167,7 +167,7 @@ export default function TinymceEditor({
               onClick={askBot}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
-              Ask
+              Ask AI
             </button>
           </div>
         </p>
